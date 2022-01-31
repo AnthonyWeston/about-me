25b63d1cab7d8199d78412902b6752804c9bb6a9
Anthony <11080358+AnthonyWeston@users.noreply.github.com>
2022-01-30 21:40:33 -0500

    Add tab open/close animation

--- src/components/ui/VTab.vue
+++ src/components/ui/VTab.vue
@@ -1,18 +1,21 @@
 <template>
-  <v-btn
-    :class="['text-subtitle-1', 'pa-0']"
-    :ripple="false"
-    rounded="0"
-  >
-    <v-container class="ma-1">{{ name }}</v-container>
-    <v-icon
-      :v-ripple="!pinned"
-      :class="closeButtonClasses"
-      :icon="pinned ? 'mdi-pin' : 'mdi-close'"
-      size="x-small"
-      @click.stop="close"
-    />
-  </v-btn>
+  <transition name="slide">
+    <v-btn
+      v-if="show"
+      :class="['text-subtitle-1', 'pa-0']"
+      :ripple="false"
+      rounded="0"
+    >
+      <v-container class="ma-1">{{ name }}</v-container>
+      <v-icon
+        :v-ripple="!pinned"
+        :class="closeButtonClasses"
+        :icon="pinned ? 'mdi-pin' : 'mdi-close'"
+        size="x-small"
+        @click.stop="close"
+      />
+    </v-btn>
+  </transition>
 </template>
 
 <script lang="ts">
@@ -31,6 +34,11 @@ export default defineComponent({
     },
   },
   emits: ['close'],
+  data() {
+    return {
+      show: false,
+    };
+  },
   computed: {
     closeButtonClasses() {
       const classes = 'text-grey align-self-start ml-n3 mt-1 mr-1 elevation-12'.split(' ');
@@ -38,22 +46,46 @@ export default defineComponent({
       return classes;
     },
   },
+  mounted() {
+    this.show = true;
+  },
   methods: {
     close() {
-      if (!this.pinned) this.$emit('close');
+      if (!this.pinned && this.show) {
+        this.show = false;
+        setTimeout(() => this.$emit('close'), 100);
+      }
     },
   },
 });
 </script>
 
 <style lang="scss" scoped>
-
   .v-btn {
     text-transform: lowercase;
     font-weight: unset;
+    transform-origin: left;
   }
 
   .v-icon:not(.pinned):hover {
     color: #2196F3 !important;
+  }
+
+  $leave-duration: 0.15s;
+  $enter-duration: $leave-duration / 2;
+
+  .slide-enter-active {
+    transition: transform $enter-duration ease-out;
+  }
+  .slide-leave-active {
+    transition: transform $leave-duration ease-in;
+  }
+
+  .slide-enter-to, .slide-leave-from {
+    transform: scale(1, 1);
+  }
+
+  .slide-enter-from, .slide-leave-to {
+    transform: scale(0, 0);
   }
 </style>
