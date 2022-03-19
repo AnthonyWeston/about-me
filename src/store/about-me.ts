6f444f6a0b3ba6726cda2c589e6e9621c8ed2d49
Anthony <11080358+AnthonyWeston@users.noreply.github.com>
2022-03-19 17:06:49 -0400

    Reorganize about-me.ts

--- src/store/about-me.ts
+++ src/store/about-me.ts
@@ -15,9 +15,19 @@ export const details = {
   pronouns: new ContentLink(['He', 'Him', 'His'], new HoverSpec('pronouns.md', 'VPronouns')),
   occupation: 'Full-stack web developer',
   currentPosition: 'Senior Developer - State of Ohio',
-  // askMeAbout: [],
-  featuredProjects: ['404 Keybinding Not Found (VSCode)', 'Budget Sample App (Angular/Ionic)'],
   goals: ['Tech lead'],
+  technologies: {
+    languages: new ContentLink(['Java', 'TypeScript', 'Bash', 'Python', 'Ruby', 'C#'], new TabSpec('chart.js', 'VDonutChart')),
+    backend: ['Spring Boot MVC', 'Node.js', 'Django'],
+    frontend: ['Vue 2 / Vue 3', 'Vuetify', 'Angular', 'Material Design'],
+    buildTools: ['Babel/Webpack', 'Maven', 'Docker'],
+    devOps: ['GitLab CI', 'GitHub CI', 'Jenkins', 'SonarQube'],
+    codeQuality: ['SonarQube', 'ESLint', 'Checkstyle'],
+    testing: ['JUnit 5', 'Jest', 'Cucumber', 'Selenium'],
+    database: ['PostgresQL', 'SQL Server', 'Oracle'],
+    misc: ['git', 'NginX', 'Linux', 'Jackson', 'Ionic', 'ProseMirror', 'AWS'],
+  },
+  featuredProjects: ['404 Keybinding Not Found (VSCode)'],
   learning: {
     current: ['Vue 3', 'Java 17', 'Test automation'],
     interests: [
@@ -27,18 +37,7 @@ export const details = {
       'Test automation',
     ],
   },
-  technologies: {
-    languages: new ContentLink(['Java', 'TypeScript', 'Bash', 'Python', 'Ruby', 'C#'], new TabSpec('chart.js', 'VDonutChart')),
-    backend: ['Spring Boot MVC', 'Django'],
-    frontend: ['Vue', 'Angular'],
-    mobile: ['Ionic'],
-    testing: ['JUnit 5', 'Jest'],
-    tools: ['Babel/Webpack', 'Maven'],
-    database: ['PostgresQL', 'SQL Server', 'Oracle'],
-    cloud: ['AWS'],
-    devOps: ['GitLab CI', 'GitHub CI', 'Jenkins', 'SonarQube'],
-    misc: ['git', 'Docker', 'NginX', 'Linux', 'ProseMirror', 'Jackson'],
-  },
+
   architecture: ['Vertical slice', 'Microservices', 'Domain-driven design', 'Event-driven design'],
   otherInterests: ['Coffee', 'Board Games', 'Tabletop RPGs'],
 };
