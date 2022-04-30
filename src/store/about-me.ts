fc2bc0d72ba2d477995bcb6494ef36ca872380d1
Anthony <11080358+AnthonyWeston@users.noreply.github.com>
2022-04-30 17:15:10 -0400

    Update about-me.ts

--- src/store/about-me.ts
+++ src/store/about-me.ts
@@ -20,25 +20,18 @@ export const details = {
     languages: new ContentLink(['Java', 'TypeScript', 'Bash', 'Python', 'Ruby', 'C#'], new TabSpec('chart.js', 'VDonutChart')),
     backend: ['Spring Boot MVC', 'Node.js', 'Django', '.NET'],
     frontend: ['Vue 2 / Vue 3', 'Vuetify', 'Angular', 'Material Design'],
-    buildTools: ['Babel/Webpack', 'Maven', 'Docker'],
     devOps: ['GitLab CI', 'Jenkins', 'SonarQube'],
+    buildTools: ['Babel/Webpack', 'Maven', 'Docker'],
     codeQuality: ['SonarQube', 'ESLint', 'Checkstyle'],
     testing: ['JUnit 5', 'Jest', 'Cucumber', 'Selenium'],
     database: ['PostgresQL', 'SQL Server', 'Oracle'],
     misc: ['git', 'NginX', 'Linux', 'Jackson', 'Ionic', 'ProseMirror', 'AWS'],
   },
-  featuredProjects: ['404 Keybinding Not Found (VSCode)'],
+  featuredProjects: ['About Me (this website)', '404 Keybinding Not Found (VSCode)'],
   learning: {
-    current: ['Vue 3', 'Java 17', 'Test automation'],
-    interests: [
-      'Code quality',
-      'Cross-platform development',
-      'CI/CD',
-      'Test automation',
-    ],
+    current: ['Event-driven architecture, Domain-driven design'],
+    interests: ['Code quality', 'Cross-platform development', 'Test automation'],
   },
-
-  architecture: ['Vertical slice architecture', 'Microservices', 'Domain-driven design', 'Event-driven design'],
   otherInterests: ['Coffee', 'Board Games', 'Tabletop RPGs'],
 };
 
